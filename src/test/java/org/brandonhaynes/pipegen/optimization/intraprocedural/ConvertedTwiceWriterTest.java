8c2dc894e329891bc7442366df0144f0737dc850
Brandon Haynes <bhaynes@cs.washington.edu>
2016-06-28 07:28:43 -0700

    Additional tests

--- src/test/java/org/brandonhaynes/pipegen/optimization/intraprocedural/ConvertedTwiceWriterTest.java
+++ src/test/java/org/brandonhaynes/pipegen/optimization/intraprocedural/ConvertedTwiceWriterTest.java
@@ -1,17 +1,23 @@
 package org.brandonhaynes.pipegen.optimization.intraprocedural;
 
+import org.brandonhaynes.pipegen.instrumentation.injected.java.AugmentedString;
 import org.brandonhaynes.pipegen.optimization.OptimizationTest;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class PrimitiveWriterTest extends OptimizationTest  {
+public class ConvertedTwiceWriterTest extends OptimizationTest  {
     @Override
     public void test() throws IOException {
         OutputStreamWriter writer = getTestWriter();
 
         int primitive = 5;
         String converted = Integer.toString(primitive);
-        writer.write(converted);
+        assert(converted instanceof AugmentedString);
+
+        String convertedAgain = converted.toString();
+        assert(convertedAgain instanceof AugmentedString);
+
+        writer.write(convertedAgain);
     }
 }
