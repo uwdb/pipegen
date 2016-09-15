b9ecfd8ed65f30f1302b6965e20b6a0cf7993021
Brandon Haynes <bhaynes@cs.washington.edu>
2016-09-15 08:57:22 -0700

    Extract JVM prep into script

---
+++ bin/prepare-jvm.sh
@@ -0,0 +1,16 @@
+MAVEN_REPOSITORY=$HOME/.m2/repository
+BUILD_DIR=.
+TARGET_DIR=./stage
+
+mkdir $TARGET_DIR
+mvn dependency:resolve
+
+$JAVA_HOME/bin/javac -classpath "$MAVEN_REPOSITORY/org/javassist/javassist/3.20.0-GA/javassist-3.20.0-GA.jar:$MAVEN_REPOSITORY/com/google/guava/guava/18.0/guava-18.0.jar" \
+           -d $TARGET_DIR \
+           $BUILD_DIR/src/main/java/org/brandonhaynes/pipegen/utilities/JvmUtilities.java \
+           $BUILD_DIR/src/main/java/org/brandonhaynes/pipegen/mutation/ClassModifierReplacer.java \
+           $BUILD_DIR/src/main/java/org/brandonhaynes/pipegen/utilities/JarUtilities.java \
+           $BUILD_DIR/src/main/java/org/brandonhaynes/pipegen/configuration/Version.java \
+           $BUILD_DIR/src/main/java/org/brandonhaynes/pipegen/utilities/JarClassPath.java
+$JAVA_HOME/bin/java -classpath "$TARGET_DIR:$MAVEN_REPOSITORY/org/javassist/javassist/3.20.0-GA/javassist-3.20.0-GA.jar:$MAVEN_REPOSITORY/com/google/guava/guava/18.0/guava-18.0.jar" \
+          org.brandonhaynes.pipegen.utilities.JvmUtilities RemoveFinalFlagFromString java.lang.String,java.lang.StringBuilder,java.lang.StringBuffer
