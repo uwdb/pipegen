3242aad34b21bf54d279e1b5a7f7226c3c48e333
Brandon Haynes <bhaynes@cs.washington.edu>
2016-09-16 13:59:09 -0700

    Separate prepare invocations

--- bin/prepare-jvm.sh
+++ bin/prepare-jvm.sh
@@ -3,7 +3,6 @@ MAVEN_REPOSITORY=$HOME/.m2/repository
 BUILD_DIR=.
 TARGET_DIR=./stage
 
-pwd
 mkdir $TARGET_DIR
 mvn dependency:resolve
 
@@ -15,4 +14,8 @@ $JAVA_HOME/bin/javac -classpath "$MAVEN_REPOSITORY/org/javassist/javassist/3.20.
            $BUILD_DIR/src/main/java/org/brandonhaynes/pipegen/configuration/Version.java \
            $BUILD_DIR/src/main/java/org/brandonhaynes/pipegen/utilities/JarClassPath.java
 sudo $JAVA_HOME/bin/java -classpath "$TARGET_DIR:$MAVEN_REPOSITORY/org/javassist/javassist/3.20.0-GA/javassist-3.20.0-GA.jar:$MAVEN_REPOSITORY/com/google/guava/guava/18.0/guava-18.0.jar" \
-          org.brandonhaynes.pipegen.utilities.JvmUtilities RemoveFinalFlagFromString java.lang.String,java.lang.StringBuilder,java.lang.StringBuffer
+          org.brandonhaynes.pipegen.utilities.JvmUtilities RemoveFinalFlagFromString java.lang.String
+sudo $JAVA_HOME/bin/java -classpath "$TARGET_DIR:$MAVEN_REPOSITORY/org/javassist/javassist/3.20.0-GA/javassist-3.20.0-GA.jar:$MAVEN_REPOSITORY/com/google/guava/guava/18.0/guava-18.0.jar" \
+          org.brandonhaynes.pipegen.utilities.JvmUtilities RemoveFinalFlagFromString java.lang.StringBuilder
+sudo $JAVA_HOME/bin/java -classpath "$TARGET_DIR:$MAVEN_REPOSITORY/org/javassist/javassist/3.20.0-GA/javassist-3.20.0-GA.jar:$MAVEN_REPOSITORY/com/google/guava/guava/18.0/guava-18.0.jar" \
+          org.brandonhaynes.pipegen.utilities.JvmUtilities RemoveFinalFlagFromString java.lang.StringBuffer
