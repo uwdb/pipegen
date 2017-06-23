095faf9f5f5d765e36e838cd606b1cc68901f35a
Yingkai Wang <yingkaiwangd@gmail.com>
2017-06-23 11:42:50 -0700

    also update pipegen.c in src main c

--- src/main/c/pipegen.h
+++ src/main/c/pipegen.h
@@ -9,10 +9,9 @@
 #include <curl/curl.h>
 #include <unistd.h>
 
-#define PIPEGEN_SOCKET_NAME "pipegen.csv"
 #define PIPEGEN_SOCKET_HOST "127.0.0.1"
-#define PIPEGEN_SOCKET_PORT 8889
-#define PIPEGEN_SOCKET_PORT_STR "8889"
+#define PIPEGEN_SOCKET_LISTEN_RETRY_TIME 5
+#define PIPEGEN_SOCKET_PORT_BASE 8000
 #define PIPEGEN_DIRECTORY_ADDRESS "127.0.0.1:8888/"
 #define PIPEGEN_DIRECTORY_EXPORT_SUB "export?"
 #define PIPEGEN_DIRECTORY_IMPORT_SUB "import?"
@@ -30,6 +29,8 @@ struct DB_ADDRESS {
 	char *port;
 };
 
-FILE *pipegen_fopen(const char *path, const char *mode);
+FILE *pipegen_fopen_st(char *path, char *mode);
+
+unsigned long pipegen_fopen(unsigned long path, unsigned long mode);
 
 #endif /* #ifndef _PIPIEGEN_H_ */
